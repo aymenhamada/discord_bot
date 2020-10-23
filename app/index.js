fcb31e09634e46926366f15ae51a47df23454d71
barrosok <kbarroso@clinflows.com>
2020-10-24 00:49:43 +0200

    - fixed interval

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ dotenv.config();
 import client from './service/client.js';
 import {onMessage, onReady} from './event/index.js';
 
+// eslint-disable-next-line no-unused-vars
+import interval from '../app/builder/intervalBuilder.js';
 
 client.login(process.env.DISCORD_TOKEN);
 client.on('ready', onReady);
--- app/intervals/bonjour.js
+++ app/intervals/bonjour.js
@@ -3,7 +3,7 @@ export default {
     interval: 1000 * 10, // 10 seconds
     guilds: [
         {id: '767081989920522250', channels: ['767081989920522254']}, // serveur barroso
-        {id: '742440123383414825', channels: ['767735685968691201']} // serveur weber
+        // {id: '742440123383414825', channels: ['767735685968691201']} // serveur weber
     ],
     handler: (guild, channel) => {
         channel.send('bonjour');
